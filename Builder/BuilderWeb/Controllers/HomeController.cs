7e867c712c19d6af5d50fba0bfee8a4a8a35a9fe
Nataly Hernandez <natymony@hotmail.com>
2022-10-20 18:36:22 -0500

    front

---
+++ Builder/BuilderWeb/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using BuilderWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace BuilderWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult>  Index(IFormCollection form)
+        {
+            string activo = form["listadoAct"].ToString();
+            activos act = new activos();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://localhost:44362/Builder?activo=" + activo))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    act = JsonConvert.DeserializeObject<activos>(apiResponse);
+                }
+            }
+            act.activo = activo;
+            return View(act);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Builder/BuilderWeb/Models/activos.cs
@@ -0,0 +1,18 @@
+﻿namespace BuilderWeb.Models
+{
+    public class activos
+    {
+        public string activo { get; set; }
+        public int codigo { get; set; }
+        public string? nombre { get; set; }
+        public double precio { get; set; }
+        public string? departamento { get; set; } //auto, computador
+        public string? anioDepreciacion { get; set; } // auto, computador
+        public int? añoFabricacion { get; set; } //auto
+        public string? tipo { get; set; } //computador
+        public string? fechaDeCompra { get; set; } //computador
+        public string? responsable { get; set; }  //computador
+        public string? placa { get; set; }  //computador
+
+    }
+}
