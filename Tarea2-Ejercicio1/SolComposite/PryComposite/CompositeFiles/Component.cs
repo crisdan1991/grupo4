f9aff3b31825095e64f4ff099def62de6698e60e
Jhony Cristian Llano Tumbaco <cristiandan_1991@hotmail.com>
2022-10-27 08:33:29 -0500

    Add files via upload
    backEjercicio2

---
+++ Tarea2-Ejercicio1/SolComposite/PryComposite/CompositeFiles/Component.cs
@@ -0,0 +1,20 @@
+﻿namespace PryComposite.CompositeFiles
+{
+    public abstract class Component
+    {
+        string _nombre;
+
+        public Component(string nombre)
+        {
+            _nombre = nombre;
+        }
+
+        public string Nombre { get { return _nombre; } }
+
+        public abstract void AgregarHijo(Component component);
+
+        public abstract IList<Component> ObtenerHijos();
+
+        public abstract int ObtenerTamanio { get;  }
+    }
+}
